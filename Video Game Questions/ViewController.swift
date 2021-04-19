6f8751623cb391f735c4cb987ccfe1dcb41bc240
Jonathan Rhea <indianarhea@gmail.com>
2021-04-18 23:52:10 -0400

    End of day 4/18

--- Video Game Questions/ViewController.swift
+++ Video Game Questions/ViewController.swift
@@ -10,7 +10,8 @@ import SwiftUI
 import Foundation
 
 class ViewController: UIViewController {
-
+    
+    //link buttons and labels with varaibles
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerButton1: UIButton!
     @IBOutlet weak var answerButton2: UIButton!
@@ -18,7 +19,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerButton4: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
     
-    
+    //varaibles and arrays needed
     var questionsArray : [String] = []
     var questionOrder = Array(repeating: 0, count: 5)
     var questionLine : [Substring] = []
@@ -38,14 +39,19 @@ class ViewController: UIViewController {
     
     func readQuestion(){
         
+        //put 0 through 4 in this order array
+        //this is needed to randomize the questions later
         for counter in 0...4{
             
             questionOrder[counter] = counter
             
         }//end for
         
+        //array of questions
         questionsArray = ["Which Wii game was a launch title and the best selling game for the system?,Wii Sports,The Legend of Zelda: Twilight Princess,Wii Play,Super Mario Galaxy," , "What is Mario's first 3D adventure?,Super Mario 64,Super Mario Sunshine,Mario Party,Mario Tennis," , "Which Pokemon game takes place in the Sinnoh region?,Pokemon Platinum,Pokemon Sword/Shield,Pokemon Heart Gold/Soul Silver,Pokemon Emerald," , "Who is Joker's main persona in Persona 5?,Arsene,Izanagi,Captain Kidd,Carmen," , "Who is Mario trying to save in the original Donkey Kong game?,Pauline,Princess Peach,Princess Daisy,Rosalina,"]
         
+        //shuffle the questionOrder array
+        //this is how the questions get randomized
         questionOrder.shuffle()
        
         
@@ -54,44 +60,70 @@ class ViewController: UIViewController {
     
     func setTexts(){
         
-        
+        //get a random question by moving through the index of the randomized questionOrder one by one
         questionLine = questionsArray[questionOrder[questionCounter]].split(separator:",")
+        
+        //first answer listed in questionLine is the correct answer
         correctAnswer = questionLine[1]
          
+        //put answers into answerArray
         answerArray[0] = questionLine[1]
         answerArray[1] = questionLine[2]
         answerArray[2] = questionLine[3]
         answerArray[3] = questionLine[4]
+        
+        //shuffle the answers
         answerArray.shuffle()
         
+        //set the question and button texts
         questionLabel.text = String(questionLine[0])
         answerButton1.setTitle(String(answerArray[0]), for: .normal)
         answerButton2.setTitle(String(answerArray[1]), for: .normal)
         answerButton3.setTitle(String(answerArray[2]), for: .normal)
         answerButton4.setTitle(String(answerArray[3]), for: .normal)
 
-         questionCounter+=1
+        questionCounter+=1
         
         
     }//end setTexts
 
+    //methods for all 4 buttons are the same
+    //they just correspond to the different buttons
     @IBAction func answer1Button(_ sender: Any) {
+        //if correct, display correct and disable buttons
         if(answerArray[0] == correctAnswer){
             self.resultLabel.text = "Correct!"
             resultLabel.textColor = UIColor.green
-            print("Correct!")
             correctCoutner+=1
-            setTexts()
+            answerButton1.isEnabled = false
+            answerButton2.isEnabled = false
+            answerButton3.isEnabled = false
+            answerButton4.isEnabled = false
+            
+            //set a three second delay, then get another question
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.resultLabel.text = " "
+                self.setTexts()
+                self.answerButton1.isEnabled = true
+                self.answerButton2.isEnabled = true
+                self.answerButton3.isEnabled = true
+                self.answerButton4.isEnabled = true
+            }
             
         }//end if
         else{
+            //if incorrect, display incorrect and disable buttons
             resultLabel.text = "Incorrect!"
             resultLabel.textColor = UIColor.red
             answerButton1.isEnabled = false
             answerButton2.isEnabled = false
             answerButton3.isEnabled = false
             answerButton4.isEnabled = false
-            print("Incorrect!")
+            
+            //set a three second delay, then display amount of questions that were answered correctly
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.resultLabel.text = "You got " + String(self.correctCoutner) + " correct!"
+            }
         }//end if
             
     }//end answer1Button
@@ -100,10 +132,20 @@ class ViewController: UIViewController {
         if(answerArray[1] == correctAnswer){
             self.resultLabel.text = "Correct!"
             resultLabel.textColor = UIColor.green
-            print("Correct!")
             correctCoutner+=1
-            setTexts()
+            answerButton1.isEnabled = false
+            answerButton2.isEnabled = false
+            answerButton3.isEnabled = false
+            answerButton4.isEnabled = false
             
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.resultLabel.text = " "
+                self.setTexts()
+                self.answerButton1.isEnabled = true
+                self.answerButton2.isEnabled = true
+                self.answerButton3.isEnabled = true
+                self.answerButton4.isEnabled = true
+            }
         }//end if
         else{
             resultLabel.text = "Incorrect!"
@@ -112,7 +154,10 @@ class ViewController: UIViewController {
             answerButton2.isEnabled = false
             answerButton3.isEnabled = false
             answerButton4.isEnabled = false
-            print("Incorrect!")
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.resultLabel.text = "You got " + String(self.correctCoutner) + " correct!"
+            }
         }//end if
 
     }//end answer2Button
@@ -121,10 +166,20 @@ class ViewController: UIViewController {
         if(answerArray[2] == correctAnswer){
             self.resultLabel.text = "Correct!"
             resultLabel.textColor = UIColor.green
-            print("Correct!")
             correctCoutner+=1
-            setTexts()
+            answerButton1.isEnabled = false
+            answerButton2.isEnabled = false
+            answerButton3.isEnabled = false
+            answerButton4.isEnabled = false
             
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.resultLabel.text = " "
+                self.setTexts()
+                self.answerButton1.isEnabled = true
+                self.answerButton2.isEnabled = true
+                self.answerButton3.isEnabled = true
+                self.answerButton4.isEnabled = true
+            }
         }//end if
         else{
             resultLabel.text = "Incorrect!"
@@ -133,7 +188,10 @@ class ViewController: UIViewController {
             answerButton2.isEnabled = false
             answerButton3.isEnabled = false
             answerButton4.isEnabled = false
-            print("Incorrect!")
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.resultLabel.text = "You got " + String(self.correctCoutner) + " correct!"
+            }
         }//end if
 
     }//end answer3Button
@@ -142,10 +200,20 @@ class ViewController: UIViewController {
         if(answerArray[3] == correctAnswer){
             self.resultLabel.text = "Correct!"
             resultLabel.textColor = UIColor.green
-            print("Correct!")
             correctCoutner+=1
-            setTexts()
+            answerButton1.isEnabled = false
+            answerButton2.isEnabled = false
+            answerButton3.isEnabled = false
+            answerButton4.isEnabled = false
             
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.resultLabel.text = " "
+                self.setTexts()
+                self.answerButton1.isEnabled = true
+                self.answerButton2.isEnabled = true
+                self.answerButton3.isEnabled = true
+                self.answerButton4.isEnabled = true
+            }
         }//end if
         else{
             resultLabel.text = "Incorrect!"
@@ -154,7 +222,10 @@ class ViewController: UIViewController {
             answerButton2.isEnabled = false
             answerButton3.isEnabled = false
             answerButton4.isEnabled = false
-            print("Incorrect!")
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.resultLabel.text = "You got " + String(self.correctCoutner) + " correct!"
+            }
         }//end if
 
     }//end answer4Button
